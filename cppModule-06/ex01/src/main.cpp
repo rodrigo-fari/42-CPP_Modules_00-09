0e2b10f0a1a5e045c6f1e8123c38b4a4d6c3f775
Rodrigo Faria <rodrigofal44@gmail.com>
2026-03-17 11:57:45 +0000

    Started CPP06EX01.

--- cppModule-06/ex01/src/main.cpp
+++ cppModule-06/ex01/src/main.cpp
@@ -6,17 +6,7 @@
 /*   By: rde-fari <rde-fari@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/26 21:18:50 by rde-fari          #+#    #+#             */
-/*   Updated: 2026/03/17 11:54:57 by rde-fari         ###   ########.fr       */
+/*   Updated: 2026/03/17 11:57:18 by rde-fari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-#include "ScalarConverter.hpp"
-
-int main(int ac, char **av)
-{
-	if (ac != 2)
-		return ((std::cerr << "[ERROR: Invalid usage.]\nUsage: ./ex00 <Only one argument>.\n"), 1);
-	std::string inputData = av[1];
-	ScalarConverter::convert(inputData);
-}
 
