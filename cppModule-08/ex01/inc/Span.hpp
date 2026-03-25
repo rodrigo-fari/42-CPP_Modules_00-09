3ed707b340d4ba386a4797cab1e89e3ab373d0d1
Rodrigo Falcao <rodrigofal44@gmail.com>
2026-03-25 14:51:59 +0000

    CPP08EX01 WIP. Basic project structure done.

--- cppModule-08/ex01/inc/Span.hpp
+++ cppModule-08/ex01/inc/Span.hpp
@@ -6,7 +6,7 @@
 /*   By: rde-fari <rde-fari@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/24 21:16:47 by rde-fari          #+#    #+#             */
-/*   Updated: 2026/03/24 21:38:55 by rde-fari         ###   ########.fr       */
+/*   Updated: 2026/03/25 14:48:03 by rde-fari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,31 @@
 
 # include <iostream>
 # include <string>
-# include "MutantStack.tpp"
+# include <exception>
+# include <vector>
+
+class Span {
+	private:
+		unsigned int _n;
+		std::vector<unsigned int> numberVector;
+
+	public:
+		Span();
+		Span(unsigned int n);
+		Span& operator=(const Span& other);
+		~Span();
+
+		void addNumber(unsigned int number);
+		
+		unsigned int shortestSpan();
+		unsigned int longestSpan();
+
+		class OutOfBoundesException : public std::exception {
+			public:
+				const char* what() const throw();	
+		};
+};
+
+std::ostream& operator<<(std::ostream& out, const Span& rhs);
 
 #endif
--- cppModule-08/ex01/src/Span.cpp
+++ cppModule-08/ex01/src/Span.cpp
@@ -6,9 +6,40 @@
 /*   By: rde-fari <rde-fari@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/24 21:18:12 by rde-fari          #+#    #+#             */
-/*   Updated: 2026/03/24 21:20:24 by rde-fari         ###   ########.fr       */
+/*   Updated: 2026/03/25 14:50:57 by rde-fari         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Span.hpp"
 
+Span::Span() : _n(0) { }
+
+Span::Span( unsigned int number ) : _n( number ) { }
+
+Span& Span::operator=( const Span& other ) {
+	if ( this != &other )
+		this->numberVector = other.numberVector;
+	return ( *this );	
+}
+
+Span::~Span() { }
+
+void Span::addNumber( unsigned int number ) {
+	
+}
+
+unsigned int Span::shortestSpan() {
+	
+}
+
+unsigned int Span::longestSpan() {
+	
+}
+
+const char* Span::OutOfBoundesException::what() const throw() {
+	return ( "Error: Out of bounds!" );
+}
+
+std::ostream& operator<<(std::ostream out, const Span& rhs) {
+	//retorna talvez o vetor, ou o limite determinado por N
+}
